2e9a9ade584b4954f59091c9b39c7317dd18f969
Harshit Anand <harshitanand283@gmail.com>
2023-01-02 23:28:20 +0530

    Login: sign up form css updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,10 @@ function Login() {
 				<input type="email" placeholder="Email" />
 				<input type="password" placeholder="Password" />
 				<button type="submit">Sign In</button>
+				<h4>
+					<span className="login__gray">New to Netflix? </span>
+					<span className="login__link">Sign Up now.</span>
+				</h4>
 			</form>
 		</div>
 	);
